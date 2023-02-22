951c66dee06989f9f9e3b25ca0a66e2fe9cbfd2e
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2023-02-22 13:00:37 -0700

    initial commit

---
+++ src/components/PollList.svelte
@@ -0,0 +1,19 @@
+<script>
+  import PollStore from '../stores/PollStore'
+  import PollDetails from './PollDetails.svelte'
+
+</script>
+
+<div class="poll-list">
+  {#each $PollStore as poll (poll.id)}
+    <PollDetails {poll} />
+  {/each}
+</div>
+
+<style>
+  .poll-list{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+</style>
